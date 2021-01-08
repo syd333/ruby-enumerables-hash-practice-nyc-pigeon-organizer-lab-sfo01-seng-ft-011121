cc377b070c8aaa4be7d7b87e119545e0b707e200
sydney miranda <sydmiranda@icloud.com>
2021-01-08 20:27:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,6 +10,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
         end
+        if !final_array[name][key] = []
       end
       end
     final_array
