e02e82bab3a309688bcb183e216a3b2db1985151
sydney miranda <sydmiranda@icloud.com>
2021-01-08 20:21:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, value), final_array|
-    binding.pry
+    #binding.pry
     value.each do |inner_key,names|
       binding.pry
     final_array
